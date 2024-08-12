3a8fbcd935f2254b40e3ebefb6b51f97e7aea9a4
ibro <sameoldlab@proton.me>
2024-08-12 01:37:09 +0100

    update tests

--- packages/ui/src/createFractl2.test.svelte.ts
+++ packages/ui/src/createFractl2.test.svelte.ts
@@ -1,17 +1,17 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { createFractl2 } from './createFractl2.svelte.js'
+import { createFractl } from './createFractl.svelte.js'
 import { eip155 } from '@fractl-ui/evm'
 import { mockConfig } from '../lib/wagmiConfig.js'
+import { get } from 'svelte/store'
 
 describe('FiniteStateMachine', () => {
 	describe('simple toggle switch', () => {
-		let fctl = createFractl2({
+		let fctl = createFractl({
 			namespaces: [eip155(mockConfig)]
 		})
-		const life = fctl.help()
 
 		/* beforeEach(() => {
-			fctl = createFractl2({
+			fctl = createFractl({
 				namespaces: [eip155(mockConfig)]
 			})
 		}) */
@@ -27,33 +27,40 @@ describe('FiniteStateMachine', () => {
 
 		it('switches states while connecting', async () => {
 			const connectors = fctl.connectors
-			// console.log('CONNECTORS: ', connectors)
-			const connecting = fctl.connect(connectors[0][1])
+			const connecting = fctl.connect(connectors[0])
 			expect(fctl.state).toBe('connecting')
 			await connecting
 			expect(fctl.state).toBe('connected')
 		})
 
-		const connection = fctl.connections.entries().next()
 		it('Lists active connections', () => {
-			expect(connection).not.toBeUndefined()
+			expect(fctl.connections.entries().next().value).not.toBeUndefined()
 		})
 		//add another test when I figure out how to represent connections
 
-		it("returns a given account's data", () => {
-			const address = connection.address
-			const data = fctl.call('watchAccount', {
-				address
+		it("returns a given account's data", async () => {
+			const connection = fctl.connections.values().next().value
+			const address = connection.addresses[0]
+			const namespace = connection.chain_id.namespace
+			const data = fctl.call({
+				action: 'watchAccount',
+				namespace,
+				args: address
 			})
-			expect(data).toHaveProperty('name')
-
+			expect(data).toHaveProperty('subscribe')
+			expect(get(data).address).toEqual(address)
 			// should wait for network here somehow?
 			// and also something less brittle than V's ENS
-			expect(data.name).toBe('vitalik.eth')
+			await expect
+				.poll(() => get(data).name, { timeout: 5000 })
+				.toBe('vitalik.eth')
 		})
 
-		it('changes state when disconnected', () => {
-			expect(fctl.disconnect()).toBe('disconnected')
+		it('changes state when disconnected', async () => {
+			expect(fctl.state).toBe('connected')
+			const res = fctl.disconnect()
+			await res
+			expect(fctl.state).toBe('disconnected')
 		})
 	})
 })
